dd0d2763c3e779ca0ec4e1ea061a8d4317bf7d4f
kyungwon oh <39756239+kyungwon-dev@users.noreply.github.com>
2022-07-06 13:48:47 +0900

    [Bronze V] Title: 사칙연산, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int N,M;
+    cin >> N >> M;
+    cout << N + M << "\n";
+    cout << N - M << "\n";
+    cout << N * M << "\n";
+    cout << N / M << "\n";
+    cout << N % M << "\n";
+    
+    return 0;
+}

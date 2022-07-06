ff70860bd61dd1cf364756874e94165a06edcb43
kyungwon oh <39756239+kyungwon-dev@users.noreply.github.com>
2022-07-06 14:07:23 +0900

    [Silver III] Title: N과 M (6), Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/15655. N과 M （6）/N과 M （6）.cc
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+
+void go(vector<int> &numbers, int n, int m, 
+vector<int> &rs, vector<bool> &isCheck,  int cnt, int idx){
+    if(cnt == m){
+        for(auto r : rs)
+            cout << r << " ";
+        cout << "\n";
+        return;
+    }
+    for(int i=idx;i<numbers.size();i++){
+        if(isCheck[i] == false){
+            isCheck[i] = true;
+            rs.push_back(numbers[i]);
+            go(numbers, n, m, rs, isCheck, cnt+1, i+1);
+            isCheck[i] = false;
+            rs.pop_back();
+        }
+    }
+
+
+
+}
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int n, m;
+    cin >> n >> m;
+    vector<int> numbers(n);
+    vector<bool> isCheck(n);
+    for(auto &num : numbers)
+        cin >> num ;
+    sort(numbers.begin(),numbers.end()); 
+    vector<int> rs;
+    go(numbers, n, m, rs, isCheck, 0, 0);
+    
+    return 0;
+}

2b544ced075ba79bb0054d4a089aac120baa247e
kyungwon oh <39756239+kyungwon-dev@users.noreply.github.com>
2022-07-06 13:49:43 +0900

    [Bronze V] Title: A+B - 3, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int T;
+    cin >> T;
+    for(int i=0;i<T;i++){
+        int N,M;
+        cin >> N >> M;
+        cout << N+M << "\n";
+    }
+    return 0;
+}

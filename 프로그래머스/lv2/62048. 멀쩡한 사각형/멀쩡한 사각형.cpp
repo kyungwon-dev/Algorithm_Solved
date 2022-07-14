8b91d5505ea39d7ce027d74fcbde03384a1012a1
kyungwon oh <39756239+kyungwon-dev@users.noreply.github.com>
2022-07-14 19:56:38 +0900

    [level 2] Title: 멀쩡한 사각형, Time: 0.01 ms, Memory: 4.18 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/62048. 멀쩡한 사각형/멀쩡한 사각형.cpp
@@ -0,0 +1,20 @@
+#include <cmath>
+using namespace std;
+
+long long gcd(int a, int b){
+    if(b==0)
+        return a;
+    return gcd(b,a%b);
+}
+
+long long solution(int w,int h) {
+    long long answer = (long long) w * (long long) h;
+    if(w==h)
+        return answer - w;
+    long long gcf = gcd(w,h);
+    
+    w = (long long) w / gcf; h = (long long)h / (long long)gcf;
+    answer = answer - (w + h -1) * gcf;
+    
+    return answer;
+}

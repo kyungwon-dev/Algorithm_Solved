dccc8cacd005a7a8e85199e9f27b820f274d7c2d
kyungwon oh <39756239+kyungwon-dev@users.noreply.github.com>
2022-07-14 20:05:11 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 0.01 ms, Memory: 4.18 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.cpp
@@ -0,0 +1,38 @@
+#include <string>
+#include <vector>
+#include <boost/tokenizer.hpp>
+
+using namespace std;
+
+string solution(string s) {
+    string answer = "";
+    vector<string> split_s;
+    string splits_result = "";
+    for(char c : s){
+        
+        if(c == ' '){
+            split_s.push_back(splits_result);
+            split_s.push_back(" ");
+            splits_result = "";
+        }
+        else{
+            if(splits_result.length()==0){
+                if(c <= '9')
+                    splits_result.push_back(c);    
+                else
+                    splits_result.push_back(toupper(c));    
+            }
+            else{
+                if(c <= '9')
+                    splits_result.push_back(c);    
+                else
+                    splits_result.push_back(tolower(c));    
+                
+            }
+        }
+    }
+    for(string str : split_s)
+        answer += str;
+    answer += splits_result;
+    return answer;
+}

5d72e1108a707b37251591f51e519bbb0ba70344
kyungwon oh <39756239+kyungwon-dev@users.noreply.github.com>
2022-07-14 20:04:25 +0900

    [level 2] Title: 숫자 블록, Time: 119.08 ms, Memory: 3.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12923. 숫자 블록/숫자 블록.cpp
@@ -0,0 +1,29 @@
+#include <string>
+#include <vector>
+#include <cmath>
+
+using namespace std;
+
+long long calc(long long n){
+    if(n<2)
+        return 0;
+    for(long long i=2;i<=sqrt(n);i++){
+        if(n%i==0){
+            long long rs = n/i;
+            if(rs >= 10000000)
+                continue;
+            return rs;
+        }
+    }
+    return 1;
+}
+
+
+vector<int> solution(long long begin, long long end) {
+    vector<int> answer;
+    while(begin <= end){
+        answer.push_back(calc(begin));
+        begin++;
+    }
+    return answer;
+}

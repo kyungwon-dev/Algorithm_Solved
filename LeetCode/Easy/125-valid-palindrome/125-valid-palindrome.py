e57f897ee3194eb4c6c769f85f642f78f5b2028e
kyungwon oh <39756239+kyungwon-dev@users.noreply.github.com>
2022-08-04 05:39:27 +0900

    Time: 326 ms (5.01%), Space: 19.3 MB (8.50%) - LeetHub

---
+++ LeetCode/Easy/125-valid-palindrome/125-valid-palindrome.py
@@ -0,0 +1,9 @@
+class Solution:
+    
+    def isPalindrome(self, s: str) -> bool:
+        s = [ch.lower() for ch in s if ch.isalnum()]
+        while len(s) > 1:
+            if s.pop(0) != s.pop():
+                return False
+        return True
+        

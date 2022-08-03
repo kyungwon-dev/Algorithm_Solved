30851d2ae3c8278aec810cf2a3109e4b2d36e59d
kyungwon oh <39756239+kyungwon-dev@users.noreply.github.com>
2022-08-04 06:25:59 +0900

    Time: 68 ms (28.80%), Space: 14 MB (37.24%) - LeetHub

--- LeetCode/Easy/819-most-common-word/819-most-common-word.py
+++ LeetCode/Easy/819-most-common-word/819-most-common-word.py
@@ -3,7 +3,6 @@ import re
 
 class Solution:
     def mostCommonWord(self, paragraph: str, banned: List[str]) -> str:
-        # pass
         paragraph = [param for param in re.sub(r'[^\w]', ' ', paragraph).lower().split() if param not in banned]
         return Counter(paragraph).most_common(1)[0][0]
         

97a129ed32135d1634019d5360723a658f63c208
kyungwon oh <39756239+kyungwon-dev@users.noreply.github.com>
2022-08-04 08:46:08 +0900

    Time: 151 ms (98.43%), Space: 13.9 MB (91.35%) - LeetHub

--- LeetCode/Medium/5-longest-palindromic-substring/5-longest-palindromic-substring.py
+++ LeetCode/Medium/5-longest-palindromic-substring/5-longest-palindromic-substring.py
@@ -14,7 +14,7 @@ class Solution:
         answer = ""
         for idx in range(len(s)-1):
             answer = max(answer,
-                         find_palindrom(idx, idx+1),
-                         find_palindrom(idx, idx+2),
+                         find_palindrom(idx, idx+1), # 하나의 문자에서 점점 확대
+                         find_palindrom(idx, idx+2), # 두개의 문자에서 점점 확대
                          key=len)
         return answer

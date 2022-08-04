04e2aac2962009051b2922f68e1e3274e86056b2
kyungwon oh <39756239+kyungwon-dev@users.noreply.github.com>
2022-08-04 09:13:26 +0900

    Time: 98 ms (64.60%), Space: 15.2 MB (50.50%) - LeetHub

--- LeetCode/Easy/1-two-sum/1-two-sum.py
+++ LeetCode/Easy/1-two-sum/1-two-sum.py
@@ -1,13 +1,20 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
+        nums_map = {}
+        # key : value => idx: val
         for idx, val in enumerate(nums):
-            complement = target - val
-            print(complement)
-            if complement in nums[idx+1:]:
-                return [idx, nums[idx+1:].index(complement) + idx + 1]
+            nums_map[val] = idx
+        # find target - val
+        for idx, val in enumerate(nums):
+            if target - val in nums_map and idx != nums_map[target - val]:
+                return [idx, nums_map[target - val]]
+        
+        # for idx, val in enumerate(nums):
+        #     complement = target - val
+        #     print(complement)
+        #     if complement in nums[idx+1:]:
+        #         return [idx, nums[idx+1:].index(complement) + idx + 1]
 
-        
-        
         # for i in range(len(nums)):    
         #     for j in range(i+1,len(nums)):
         #         if nums[i]+nums[j] == target:

654a44bfa94af9d6a8fd5249350a4895b613798b
kyungwon oh <39756239+kyungwon-dev@users.noreply.github.com>
2022-08-06 00:23:41 +0900

    Time: 251 ms (94.37%), Space: 15.4 MB (72.77%) - LeetHub

--- LeetCode/Easy/704-binary-search/704-binary-search.py
+++ LeetCode/Easy/704-binary-search/704-binary-search.py
@@ -1,13 +1,16 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        lp , rp = 0, len(nums)-1
-        while lp <= rp:
-            mid = (lp + rp) // 2
-            if nums[mid] == target:
-                return mid
-            elif nums[mid] < target:
-                lp = mid+1
-            else:
-                rp = mid-1
-        return -1
+        # lp , rp = 0, len(nums)-1
+        # while lp <= rp:
+        #     mid = (lp + rp) // 2
+        #     if nums[mid] == target:
+        #         return mid
+        #     elif nums[mid] < target:
+        #         lp = mid+1
+        #     else:
+        #         rp = mid-1
+        # return -1
             
+        # using bitsect
+        t = bisect.bisect_left(nums, target)
+        return t if t < len(nums) and nums[t] == target else -1

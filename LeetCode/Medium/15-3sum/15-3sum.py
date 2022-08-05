3eefd40f0c64f1284438e823d5ca11b4654055c0
kyungwon oh <39756239+kyungwon-dev@users.noreply.github.com>
2022-08-05 20:47:05 +0900

    Time: 1280 ms (55.03%), Space: 18.1 MB (72.41%) - LeetHub

--- LeetCode/Medium/15-3sum/15-3sum.py
+++ LeetCode/Medium/15-3sum/15-3sum.py
@@ -1,20 +1,29 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        answer = set()
+        answer = []
         nums.sort()
         
         for idx in range(len(nums)-2) :
-            left = idx+1
-            right = len(nums)-1
+            left, right = idx+1, len(nums)-1
+            
+            if idx > 0 and nums[idx] == nums[idx-1]:
+                continue
+            
             while left < right:
                 subsum = nums[idx] + nums[left] + nums[right]
-                if subsum <= 0:
-                    if subsum == 0:
-                        rs = tuple(sorted([nums[idx],nums[left],nums[right]]))
-                        answer.add(rs)
-                    left += 1
-                else:
+                
+                if subsum < 0:
+                    left +=1
+                elif subsum > 0:
                     right -=1
-
-        return [list(x) for x in answer]
+                else:
+                    answer.append([nums[idx], nums[left], nums[right]])
+                    while left < right and nums[left] == nums[left+1]:
+                        left+=1
+                    while left < right and nums[right] == nums[right - 1]:
+                        right-=1
+                    left +=1
+                    right -=1
+        
+        return answer
             

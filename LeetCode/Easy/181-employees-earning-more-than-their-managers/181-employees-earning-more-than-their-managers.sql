88067178395b384846fe91a85628da577a36dbbd
kyungwon oh <39756239+kyungwon-dev@users.noreply.github.com>
2022-08-06 01:28:19 +0900

    Time: 327 ms (90.74%), Space: 0B (100.00%) - LeetHub

--- LeetCode/Easy/181-employees-earning-more-than-their-managers/181-employees-earning-more-than-their-managers.sql
+++ LeetCode/Easy/181-employees-earning-more-than-their-managers/181-employees-earning-more-than-their-managers.sql
@@ -1,11 +1,16 @@
 # Write your MySQL query statement below
-select em.name as Employee
-  from (select id
-             , name
-             , salary
-             , managerId
-          from Employee
-         where 1 = 1
-           and managerId is not Null) em
-  left join Employee emp on em.managerId = emp.id
-  where em.salary > emp.salary
+# select em.name as Employee
+#   from (select id
+#              , name
+#              , salary
+#              , managerId
+#           from Employee
+#          where 1 = 1
+#            and managerId is not Null) em
+#   left join Employee emp on em.managerId = emp.id
+#   where em.salary > emp.salary
+  
+  select empee.Name as Employee
+   from Employee empee inner join Employee emper on empee.ManagerId=emper.Id
+  where empee.Salary>emper.Salary
+

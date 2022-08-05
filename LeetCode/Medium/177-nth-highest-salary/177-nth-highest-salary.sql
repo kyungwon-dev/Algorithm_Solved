052589fcafff2efd45f7ce8649e67066b71ee647
kyungwon oh <39756239+kyungwon-dev@users.noreply.github.com>
2022-08-06 04:31:32 +0900

    Time: 473 ms (48.05%), Space: 0B (100.00%) - LeetHub

---
+++ LeetCode/Medium/177-nth-highest-salary/177-nth-highest-salary.sql
@@ -0,0 +1,12 @@
+CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
+BEGIN
+  SET N = N-1;
+  
+  RETURN (
+      # Write your MySQL query statement below.
+      SELECT DISTINCT salary 
+        from Employee
+       order by salary desc
+       limit N, 1
+  );
+END

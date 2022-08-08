d03f458c948f2e9363f1a6208ad1bd99b548a212
kyungwon oh <39756239+kyungwon-dev@users.noreply.github.com>
2022-08-08 09:49:50 +0900

    [Silver IV] Title: 스택, Time: 76 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Silver/10828. 스택/스택.py
@@ -0,0 +1,26 @@
+import sys
+
+n = int(sys.stdin.readline().rstrip())
+stack = []
+for i in range(n):
+    s = sys.stdin.readline().rstrip().split()
+    cmd = s[0]
+    if cmd == "push":
+        stack.append(s[1])    
+    elif cmd == "pop":
+        if len(stack) > 0:
+            print(f"{stack.pop()}")
+        else:
+            print("-1")
+    elif cmd == "size":
+        print(len(stack))
+    elif cmd == "empty":
+        if len(stack) > 0:
+            print("0")
+        else:
+            print("1")        
+    else:
+        if len(stack) > 0:
+            print(f"{stack[-1]}")
+        else:
+            print("-1")            

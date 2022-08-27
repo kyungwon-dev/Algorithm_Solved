551c5fab4f6aa4f7da04a7372613293279177418
kyungwon oh <39756239+kyungwon-dev@users.noreply.github.com>
2022-08-27 16:44:28 +0900

    [unrated] Title: 성격 유형 검사하기, Time: 0.13 ms, Memory: 10 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/118666. 성격 유형 검사하기/성격 유형 검사하기.py
@@ -0,0 +1,22 @@
+def solution(surveys, choices):
+    answer = ''
+    character = ["RT","CF","JM","AN"]
+    character_type = {1:"RT", 2:"CF", 3:"JM", 4:"AN"}
+    exam_character_type = { c:0 for ch in character for c in ch}
+    # print(exam_character_type)
+    for choice, survey in zip(choices, surveys):
+        if choice == 4:
+            continue
+        
+        if choice < 4:
+            exam_character_type[survey[0]]+= (4 - choice)
+        else:
+            exam_character_type[survey[1]]+= (choice - 4)
+
+    for idx, tp in character_type.items():
+        if exam_character_type[tp[0]] >= exam_character_type[tp[1]]:
+            answer += tp[0]
+        else:
+            answer += tp[1]
+    
+    return answer

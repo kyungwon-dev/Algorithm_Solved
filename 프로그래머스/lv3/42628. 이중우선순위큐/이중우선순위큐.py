836d5501162ce3e0df61c423207a066c82d06b63
kyungwon oh <39756239+kyungwon-dev@users.noreply.github.com>
2022-08-30 02:04:16 +0900

    [level 3] Title: 이중우선순위큐, Time: 0.04 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/lv3/42628. 이중우선순위큐/이중우선순위큐.py
+++ 프로그래머스/lv3/42628. 이중우선순위큐/이중우선순위큐.py
@@ -1,6 +1,7 @@
 import heapq
 
 def solution(operations):
+    num_dict = {}
     min_heap = []
     max_heap = []
     cnt = 0
@@ -11,20 +12,44 @@ def solution(operations):
         if operation[0] == "I":
             heapq.heappush(min_heap, operation[1] )
             heapq.heappush(max_heap, -operation[1] )
+            if operation[1] not in num_dict:
+                num_dict[operation[1]] = 1
+            else:
+                num_dict[operation[1]] += 1
             cnt += 1
         else:
             if cnt == 0:
                 continue
             if operation[1] == -1:
-                heapq.heappop(min_heap)
+                num = heapq.heappop(min_heap)
+                while num_dict[num] == 0:
+                    num = heapq.heappop(min_heap)
+                num_dict[num] -= 1    
+                
             else:
-                heapq.heappop(max_heap)
+                num = -heapq.heappop(max_heap)
+                while num_dict[num] == 0:
+                    num = -heapq.heappop(max_heap)
+                num_dict[num] -= 1    
             cnt -=1
             if cnt == 0:
-                min_heap.clear()
-                max_heap.clear()
-    # print(min_heap)
-    # print(max_heap)
+                min_heap = []
+                max_heap = []
     if cnt == 0:
         return [0,0]
-    return [-heapq.heappop(max_heap), heapq.heappop(min_heap)]
+    
+    mx = 0
+    mn = 0
+    for i in range(len(max_heap)):
+        mx = -heapq.heappop(max_heap)
+        if num_dict[mx] == 0:
+            continue
+        else:
+            break
+    for i in range(len(min_heap)):
+        mn = heapq.heappop(min_heap)
+        if num_dict[mn] == 0:
+            continue
+        else:
+            break
+    return [mx,mn]

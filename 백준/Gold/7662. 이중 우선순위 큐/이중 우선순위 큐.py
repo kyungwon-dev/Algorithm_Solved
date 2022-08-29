d099b02a4f81da2b5eb87523b22f9d3af533e8dd
kyungwon oh <39756239+kyungwon-dev@users.noreply.github.com>
2022-08-30 01:59:29 +0900

    [Gold IV] Title: 이중 우선순위 큐, Time: 6964 ms, Memory: 177476 KB -BaekjoonHub

---
+++ 백준/Gold/7662. 이중 우선순위 큐/이중 우선순위 큐.py
@@ -0,0 +1,58 @@
+import heapq
+import sys
+T = int(sys.stdin.readline().strip())
+for t in range(T):
+    N = int(sys.stdin.readline().strip())
+    num_dict = {}
+    min_heap = []
+    max_heap = []
+    cnt = 0
+    for i in range(N):
+        operation = sys.stdin.readline().strip().split()
+        operation[1] = int(operation[1])
+        
+        if operation[0] == "I":
+            heapq.heappush(min_heap, operation[1] )
+            heapq.heappush(max_heap, -operation[1] )
+            if operation[1] not in num_dict:
+                num_dict[operation[1]] = 1
+            else:
+                num_dict[operation[1]] += 1
+            cnt += 1
+        else:
+            if cnt == 0:
+                continue
+            if operation[1] == -1:
+                num = heapq.heappop(min_heap)
+                while num_dict[num] == 0:
+                    num = heapq.heappop(min_heap)
+                num_dict[num] -= 1    
+                
+            else:
+                num = -heapq.heappop(max_heap)
+                while num_dict[num] == 0:
+                    num = -heapq.heappop(max_heap)
+                num_dict[num] -= 1    
+            cnt -=1
+            if cnt == 0:
+                min_heap = []
+                max_heap = []
+    if cnt == 0:
+        print("EMPTY")
+    else:
+        mx = 0
+        mn = 0
+        for i in range(len(max_heap)):
+            mx = -heapq.heappop(max_heap)
+            if num_dict[mx] == 0:
+                continue
+            else:
+                break
+        for i in range(len(min_heap)):
+            mn = heapq.heappop(min_heap)
+            if num_dict[mn] == 0:
+                continue
+            else:
+                break
+        print(mx, mn, sep=" ")
+

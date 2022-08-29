53035128f7baf2cea7fa069b9553f0fdb62a90de
kyungwon oh <39756239+kyungwon-dev@users.noreply.github.com>
2022-08-29 21:22:50 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv3/42627. 디스크 컨트롤러/디스크 컨트롤러.py
+++ 프로그래머스/lv3/42627. 디스크 컨트롤러/디스크 컨트롤러.py
@@ -1,26 +1,3 @@
-import heapq
-from collections import deque
-
 def solution(jobs):
-    answer = process_time = 0
-    len_jobs = len(jobs)
-    jobs.sort(key= lambda x: x[0])
-    no_wait = 0
-    while jobs:
-        serve_first= []
-        for idx, [start, duration] in enumerate(jobs):    
-            if start > process_time:
-                break
-            heapq.heappush(serve_first, (duration, [idx, start, duration]))
-
-        if serve_first:
-            nx = heapq.heappop(serve_first)
-            answer = answer + nx[1][2] + (process_time - nx[1][1])
-            process_time = process_time + nx[1][2]
-
-            jobs.pop(nx[1][0])
-        else:
-            process_time = jobs[0][0]
-            no_wait += jobs[0][0] - process_time
-        
-    return int( (answer) / len_jobs)
+    answer = 0
+    return answer

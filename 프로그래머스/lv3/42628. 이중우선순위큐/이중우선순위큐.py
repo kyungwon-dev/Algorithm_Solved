5d720066da5fc0d761cce8901114c9ac1a5276bf
kyungwon oh <39756239+kyungwon-dev@users.noreply.github.com>
2022-08-30 01:05:26 +0900

    [level 3] Title: 이중우선순위큐, Time: 0.03 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv3/42628. 이중우선순위큐/이중우선순위큐.py
+++ 프로그래머스/lv3/42628. 이중우선순위큐/이중우선순위큐.py
@@ -1,22 +1,30 @@
 import heapq
 
 def solution(operations):
-    minmax_heap = []
+    min_heap = []
+    max_heap = []
+    cnt = 0
     for operation in operations:
         operation = operation.split()
-
+        operation[1] = int(operation[1])
+        
         if operation[0] == "I":
-            heapq.heappush(minmax_heap, int(operation[1]))
+            heapq.heappush(min_heap, operation[1] )
+            heapq.heappush(max_heap, -operation[1] )
+            cnt += 1
         else:
-            if not minmax_heap:
+            if cnt == 0:
                 continue
-            if int(operation[1]) == -1:
-                heapq.heappop(minmax_heap)
+            if operation[1] == -1:
+                heapq.heappop(min_heap)
             else:
-                minmax_heap.sort()
-                minmax_heap.pop()
-        # print(minmax_heap)
-    minmax_heap.sort()
-    if not minmax_heap:
+                heapq.heappop(max_heap)
+            cnt -=1
+            if cnt == 0:
+                min_heap.clear()
+                max_heap.clear()
+    # print(min_heap)
+    # print(max_heap)
+    if cnt == 0:
         return [0,0]
-    return [minmax_heap[-1], minmax_heap[0]]
+    return [-heapq.heappop(max_heap), heapq.heappop(min_heap)]

957ebc3c06f1748014c60d0bcc2405895f1d2184
kyungwon oh <39756239+kyungwon-dev@users.noreply.github.com>
2022-08-30 07:05:32 +0900

    [Bronze II] Title: Hashing, Time: 84 ms, Memory: 31436 KB -BaekjoonHub

---
+++ 백준/Bronze/15829. Hashing/Hashing.py
@@ -0,0 +1,11 @@
+import enum
+import sys
+L = int(sys.stdin.readline().strip())
+M = 1234567891
+r = [31 ** i for i in range(L)]
+a = { chr(ord('a')-1+i) : i for i in range(1, 27, 1)}
+strings = list(sys.stdin.readline().strip())
+answer = 0
+for i, string in enumerate(strings):
+    answer += r[i] * a[string]
+print(f"{answer % M}")

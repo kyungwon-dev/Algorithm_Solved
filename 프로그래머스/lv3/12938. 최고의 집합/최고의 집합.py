d1cd74c824bea073c0dd01351a9dcfec87731899
Kyungwon oh <kyungwon.dev@gmail.com>
2022-09-16 01:30:45 +0900

    modified:   프로그래머스/lv3/12938. 최고의 집합/최고의 집합.py

--- 프로그래머스/lv3/12938. 최고의 집합/최고의 집합.py
+++ 프로그래머스/lv3/12938. 최고의 집합/최고의 집합.py
@@ -11,18 +11,5 @@ def solution(n, s):
 
     for i in range(n -1, n - m -1, -1):
         answer[i] += 1
-    # print(d, m)
-#     answer = []
-#     if n==1:
-#         return [s]
-#     if answer[0] == 0:
-#         return [-1]
-#     else:
-#         answer = [d for _ in range(n)]
-#         j=1
-#         for i in range(m):
-#             answer[j] += 1
-#             j+=1
-    
     
     return answer

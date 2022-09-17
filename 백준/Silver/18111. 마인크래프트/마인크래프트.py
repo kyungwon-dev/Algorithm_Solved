abb4060bda431cc2d1113c3b966846040cb70bed
kyungwon oh <39756239+kyungwon-dev@users.noreply.github.com>
2022-09-17 22:51:21 +0900

    [Silver II] Title: 마인크래프트, Time: 368 ms, Memory: 117344 KB -BaekjoonHub

--- 백준/Silver/18111. 마인크래프트/마인크래프트.py
+++ 백준/Silver/18111. 마인크래프트/마인크래프트.py
@@ -4,11 +4,14 @@ N, M ,B = map(int, sys.stdin.readline().strip().split())
 grounds = [ list(map(int, sys.stdin.readline().strip().split())) for _ in range(N) ]
 
 answer_time, answer_height = sys.maxsize , 0
-height = 0
+height = min ( min([ min(grounds[i]) for i in range(N)]), 256)
+max_height = min ( ( B + sum([ sum(grounds[i]) for i in range(N)]) ) // ( N*M), 256)
 
-while height <= 256:
+while height <= max_height:
     plus, minus = 0, 0
     time = 0
+    bag = B
+    
     for ground in grounds:
         for g in ground:
             if g > height:

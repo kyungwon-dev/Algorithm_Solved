c7b04be98e8dbdd93f8f55183f5429badace3e86
kyungwon oh <39756239+kyungwon-dev@users.noreply.github.com>
2022-09-17 23:25:27 +0900

    [Silver II] Title: 마인크래프트, Time: 180 ms, Memory: 32296 KB -BaekjoonHub

--- 백준/Silver/18111. 마인크래프트/마인크래프트.py
+++ 백준/Silver/18111. 마인크래프트/마인크래프트.py
@@ -4,25 +4,33 @@ N, M ,B = map(int, sys.stdin.readline().strip().split())
 grounds = [ list(map(int, sys.stdin.readline().strip().split())) for _ in range(N) ]
 
 answer_time, answer_height = sys.maxsize , 0
-height = min ( min([ min(grounds[i]) for i in range(N)]), 256)
-max_height = min ( ( B + sum([ sum(grounds[i]) for i in range(N)]) ) // ( N*M), 256)
+height_count = [ 0 for _ in range(256 + 1)]
+max_height = 0
 
+for ground in grounds:
+    for g in ground:
+        height_count[g] +=1
+        max_height += g
+
+
+min_height = min( [min(ground) for ground in grounds])
+max_height = (max_height + B) // (M * N) 
+
+height = min_height
 while height <= max_height:
     plus, minus = 0, 0
     time = 0
     bag = B
     
-    for ground in grounds:
-        for g in ground:
-            if g > height:
-                minus+= (g - height)
-            else:
-                plus +=(height - g)
-    
-    if minus + B >= plus:
-        time = plus + (minus * 2)
-        if time <= answer_time:
-            answer_time = time
-            answer_height = height
+    for h in range( min_height, min(256+1, 256 + 1)):
+        if height < h:
+            time += (height_count[h]) * ( h - height) * 2
+            bag += (height_count[h]) * ( h - height)
+        else:
+            time += (height_count[h]) * ( height - h)
+            bag -= (height_count[h]) * ( height - h)
+    if time <= answer_time and bag >= 0:
+        answer_time = time
+        answer_height = height
     height +=1
 print(answer_time, answer_height, sep=" ")

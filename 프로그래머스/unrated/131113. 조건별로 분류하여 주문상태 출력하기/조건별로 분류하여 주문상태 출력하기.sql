a4c1d746c07ff6f7fa11e024ea31d1e6aed39a02
kyungwon oh <39756239+kyungwon-dev@users.noreply.github.com>
2022-10-09 18:36:43 +0900

    [unrated] Title: 조건별로 분류하여 주문상태 출력하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/unrated/131113. 조건별로 분류하여 주문상태 출력하기/조건별로 분류하여 주문상태 출력하기.sql
+++ 프로그래머스/unrated/131113. 조건별로 분류하여 주문상태 출력하기/조건별로 분류하여 주문상태 출력하기.sql
@@ -1,16 +1,2 @@
 -- 코드를 입력하세요
-SELECT ORDER_ID
-     , PRODUCT_ID
-     , OUT_DATE
-     , CASE WHEN OUT_DATE IS NULL
-            THEN "출고미정"
-            WHEN MONTH(OUT_DATE) < '5'
-            THEN "출고완료"
-            WHEN MONTH(OUT_DATE) = '5' AND DAY(OUT_DATE) <= '1'
-            THEN "출고완료"
-            ELSE "출고대기"
-       END AS 출고여부
-  FROM FOOD_ORDER
- WHERE 1 = 1
-   AND IN_DATE IS NOT NULL
-   
+SELECT

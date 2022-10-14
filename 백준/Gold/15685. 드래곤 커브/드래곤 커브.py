c86ab489b0efdd119b60eb9d7db8f2c796105ec3
kyungwon oh <39756239+kyungwon-dev@users.noreply.github.com>
2022-10-14 09:33:05 +0900

    [Gold IV] Title: 드래곤 커브, Time: 76 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Gold/15685. 드래곤 커브/드래곤 커브.py
@@ -0,0 +1,32 @@
+MAX = 110
+boards = [[False for _ in range(MAX)] for _ in range(MAX)]
+dy = [0,-1,0,1]
+dx = [1,0,-1,0]
+N = int(input())
+for i in range(N):
+    x, y, d, g = map(int, input().split())
+    boards[y][x] = True
+    move = [d]
+    for j in range(g):
+        # duplicate
+        tmp = move[:]
+        # backward
+        tmp = list(map(lambda x : (x+2) % 4, tmp))
+        # rotate
+        tmp = list(map(lambda x : x-1 if x-1 >= 0 else 3, tmp))
+        # reverse
+        tmp.reverse()
+        # append
+        move.extend(tmp)
+    
+    # move to board
+    for m in move:
+        y = y + dy[m]
+        x = x + dx[m]
+        boards[y][x] = True
+cnt = 0
+for i in range(MAX-1):
+    for j in range(MAX-1):
+        if boards[i][j] and boards[i-1][j] and boards[i][j-1] and boards[i-1][j-1]:
+            cnt+=1
+print(cnt)

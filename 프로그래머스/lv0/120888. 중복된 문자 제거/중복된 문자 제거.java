e8646ada9474c6301e012dd51fab972b937fc718
kyungwon oh <39756239+kyungwon-dev@users.noreply.github.com>
2022-10-20 08:23:50 +0900

    [level 0] Title: 중복된 문자 제거, Time: 1.31 ms, Memory: 80.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120888. 중복된 문자 제거/중복된 문자 제거.java
@@ -0,0 +1,10 @@
+class Solution {
+    public String solution(String my_string) {
+        String answer = "";
+        for(int i=0;i<my_string.length();i++){
+            if(answer.indexOf(my_string.charAt(i)) == -1)
+                answer += String.valueOf(my_string.charAt(i));
+        }
+        return answer;
+    }
+}

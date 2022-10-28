13bc955530e55c823e61468ae664e7e1d47a8d00
kyungwon oh <39756239+kyungwon-dev@users.noreply.github.com>
2022-10-28 23:51:54 +0900

    [level 0] Title: 등수 매기기, Time: 0.02 ms, Memory: 73.7 MB -BaekjoonHub

--- 프로그래머스/lv0/120882. 등수 매기기/등수 매기기.java
+++ 프로그래머스/lv0/120882. 등수 매기기/등수 매기기.java
@@ -1,15 +1,6 @@
 class Solution {
     public int[] solution(int[][] score) {
-        int[] answer = new int[score.length];
-        for(int i=0;i<score.length;i++){
-            answer[i] +=1;
-            double i_sc = (score[i][0] + score[i][1]) / 2.0;
-            for(int j=0;j<score.length;j++){
-                double sc = (score[j][0] + score[j][1]) / 2.0;
-                if(i_sc < sc)
-                    answer[i]++;
-            }
-        }
+        int[] answer = {};
         return answer;
     }
 }

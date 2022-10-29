feadad4a063ba170b20e5046b245e014ac03fce5
kyungwon oh <39756239+kyungwon-dev@users.noreply.github.com>
2022-10-29 19:19:16 +0900

    [level 0] Title: 옹알이 (1), Time: 0.27 ms, Memory: 74.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120956. 옹알이 （1）/옹알이 （1）.java
@@ -0,0 +1,43 @@
+import java.util.*;
+
+class Solution {
+    public int solution(String[] babbling) {
+        int answer = 0;
+        Map<String, String> map = new HashMap<String,String>(){{
+            put("aya","0");
+            put("ye","1");
+            put("woo","2");
+            put("ma","3");
+        }};
+        for(String bab : babbling){
+            boolean isPossible = true;
+            for(Map.Entry<String,String> set : map.entrySet()){
+                bab = bab.replace(set.getKey(),set.getValue());
+            }
+            for(int i=0;i<bab.length();i++){
+                if(i==0){
+                    if(bab.charAt(i) > '9' || bab.charAt(i) < '0'){
+                        isPossible = false;
+                        break;
+                    }        
+                }
+                else{
+                    if(bab.charAt(i) > '9' || bab.charAt(i) < '0'){
+                        isPossible = false;
+                        break;
+                    }
+                    else if(bab.charAt(i-1) == bab.charAt(i)){
+                        isPossible = false;
+                        break;
+                    }
+                }
+            }
+            if(isPossible){
+                // System.out.println(bab);
+                answer++;
+            }
+        }
+        
+        return answer;
+    }
+}

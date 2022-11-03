0762b9c203772809c6b01abe2aa3931b3bbb486a
kyungwon oh <39756239+kyungwon-dev@users.noreply.github.com>
2022-11-03 09:11:45 +0900

    [Bronze I] Title: 알고리즘 수업 - 선택 정렬 5, Time: 92 ms, Memory: 31860 KB -BaekjoonHub

--- 백준/Bronze/23899. 알고리즘 수업 － 선택 정렬 5/알고리즘 수업 － 선택 정렬 5.py
+++ 백준/Bronze/23899. 알고리즘 수업 － 선택 정렬 5/알고리즘 수업 － 선택 정렬 5.py
@@ -1,5 +1,4 @@
 import sys
-from collections import deque
 sys.setrecursionlimit(10**5)
 
 N = int(sys.stdin.readline().strip())
@@ -8,17 +7,16 @@ target = list(map(int, sys.stdin.readline().strip().split()))
 num_dict = {k:v for k,v in zip(nums, range(len(nums)))}
 nums_sort = sorted(nums)
 is_equal = 0
-
-for i in range(N-1, 0, -1):
-    if nums == target:
-        is_equal = 1
-        break
-    if nums[i] != nums_sort[i]:
-        tmp = nums[i]
-        nums[i], nums[num_dict[nums_sort[i]]] = nums[num_dict[nums_sort[i]]], nums[i]
-        num_dict[tmp], num_dict[nums[i]] = num_dict[nums[i]] ,num_dict[tmp]
-    if nums == target:
-        is_equal = 1
-        break
+if nums == target:
+    is_equal = 1
+else:
+    for i in range(N-1, 0, -1):
+        if nums[i] != nums_sort[i]:
+            tmp = nums[i]
+            nums[i], nums[num_dict[nums_sort[i]]] = nums[num_dict[nums_sort[i]]], nums[i]
+            num_dict[tmp], num_dict[nums[i]] = num_dict[nums[i]] ,num_dict[tmp]
+        if nums == target:
+            is_equal = 1
+            break
 print(is_equal)
         

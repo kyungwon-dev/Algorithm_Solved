6a3179a9d29040b01211f713a040006f1a548560
kyungwon oh <39756239+kyungwon-dev@users.noreply.github.com>
2022-11-04 04:49:31 +0900

    [Gold II] Title: 가장 긴 증가하는 부분 수열 3, Time: 1084 ms, Memory: 152356 KB -BaekjoonHub

---
+++ 백준/Gold/12738. 가장 긴 증가하는 부분 수열 3/가장 긴 증가하는 부분 수열 3.py
@@ -0,0 +1,22 @@
+import sys
+from collections import deque
+from bisect import bisect_left
+sys.setrecursionlimit(10**5)
+
+# input
+N = int(sys.stdin.readline().strip())
+nums = list(map(int,sys.stdin.readline().strip().split()))
+# dist = [1]
+lis = [nums[0]]
+for i in range(1, N):
+    if lis[-1] < nums[i]:
+        # add
+        lis.append(nums[i])
+        # dist.append(len(lis))
+    else:
+        # substitute
+        sub = bisect_left(lis, nums[i])
+        lis[sub] = nums[i]
+        # dist.append(dist[sub])
+print(len(lis))
+    

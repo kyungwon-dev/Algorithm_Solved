a093a440e2837837afc8f7f03f301ebd09cd8e8e
kyungwon oh <39756239+kyungwon-dev@users.noreply.github.com>
2022-11-06 15:15:41 +0900

    [unrated] Title: 우박수열 정적분, Time: 2.21 ms, Memory: 10.8 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/134239. 우박수열 정적분/우박수열 정적분.py
@@ -0,0 +1,22 @@
+def solution(k, ranges):
+    nums = [[0,float(k)]]
+    sums = [0]
+    while k!=1:
+        if k%2 == 0:
+            k =k / 2
+        else:
+            k =k * 3 + 1.0
+        nums.append([nums[-1][0]+1, k])
+        sums.append( sums[-1] + (nums[-1][1] + nums[-2][1]) /2 ) 
+    # print(*nums)
+    # print(*sums)
+    answer = []
+    for left, right in ranges:
+        if left+1 > len(sums) + right:
+            answer.append(-1.0)
+        else:
+            l_sum = sums[-1] - sums[left]
+            r_sum = sums[-1] - sums[len(sums)-1 + right]
+            answer.append(l_sum - r_sum if l_sum >= r_sum else -1.0)
+    
+    return answer

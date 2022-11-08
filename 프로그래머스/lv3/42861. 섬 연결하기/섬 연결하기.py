f9ff2f705121eb1f5b4553eb89b9bdd3d60e8f35
kyungwon oh <39756239+kyungwon-dev@users.noreply.github.com>
2022-11-08 22:55:28 +0900

    [level 3] Title: 섬 연결하기, Time: 0.03 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv3/42861. 섬 연결하기/섬 연결하기.py
+++ 프로그래머스/lv3/42861. 섬 연결하기/섬 연결하기.py
@@ -6,23 +6,23 @@ def solution(n, costs):
     
     bridges = []
     for cost in costs:
-        graph[cost[0]].append([cost[2], cost[0], cost[1]])
-        graph[cost[1]].append([cost[2], cost[1], cost[0]])
+        graph[cost[0]].append([cost[2], cost[1]])
+        graph[cost[1]].append([cost[2], cost[0]])
         # heapq.heappush(bridges, (cost[2], cost))
     # print(graph)
     if not cost:
         return 0
-    start = costs[0][0]
+    start = 0
     visited[start] = True
     candidate = graph[start]
     heapq.heapify(candidate)
     while candidate:
-        w, u, v = heapq.heappop(candidate)
+        w, v = heapq.heappop(candidate)
         if visited[v] == False:
             visited[v] = True
             answer += w
             for e in graph[v]:
-                if visited[e[2]] == False:
+                if visited[e[1]] == False:
                     heapq.heappush(candidate, e)
     
     

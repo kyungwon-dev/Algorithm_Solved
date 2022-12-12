1192b623463a49a714238ac5f9981780a4e295d2
kyungwon oh <39756239+kyungwon-dev@users.noreply.github.com>
2022-12-12 20:46:47 +0900

    [level 3] Title: 베스트앨범, Time: 2.65 ms, Memory: 73.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/42579. 베스트앨범/베스트앨범.java
@@ -0,0 +1,57 @@
+import java.util.*;
+
+class Solution {
+    class Music implements Comparable<Music>{
+        int idx;
+        int time;
+        public Music(int idx, int time){
+            this.idx = idx;
+            this.time = time;
+        }
+        
+        @Override
+        public int compareTo(Music m){
+            if(this.time == m.time)
+                return Integer.compare(this.idx, m.idx);
+            return -Integer.compare(this.time, m.time);
+        }
+        
+        @Override
+        public String toString(){
+            return this.idx + " " + this.time;
+        }
+    }
+    
+    public int[] solution(String[] genres, int[] plays) {
+        HashMap<String, Integer> songs = new HashMap<>();
+        HashMap<String, List<Music>> musics = new HashMap<>();
+        
+        for(int i=0;i<genres.length;i++){
+            if(songs.get(genres[i]) == null){
+                musics.put(genres[i], new ArrayList<Music>());   
+            }
+            songs.put(genres[i],songs.getOrDefault(genres[i],0)+plays[i]);
+            musics.get(genres[i]).add(new Music(i, plays[i]));
+        }
+        
+        List<String> list = new ArrayList<>(songs.keySet());
+        list.sort(new Comparator<String>(){
+            @Override
+            public int compare(String o1, String o2){
+                return -Integer.compare(songs.get(o1),songs.get(o2));
+            }
+        });
+        List<Integer> answer = new ArrayList<>();
+        
+        for(String s:list){
+            Collections.sort(musics.get(s));
+            int cnt = 0;
+            for(Music m : musics.get(s)){
+                answer.add(m.idx);
+                cnt++;
+                if(cnt == 2) break;
+            }
+        }
+        return answer.stream().mapToInt(i -> i).toArray();
+    }
+}

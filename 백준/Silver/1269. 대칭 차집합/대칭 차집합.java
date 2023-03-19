1a30327c33fbba966302ad435d90c0c86086424b
kyungwon oh <39756239+kyungwon-dev@users.noreply.github.com>
2023-03-19 16:05:31 +0900

    [Silver IV] Title: 대칭 차집합, Time: 836 ms, Memory: 83432 KB -BaekjoonHub

---
+++ 백준/Silver/1269. 대칭 차집합/대칭 차집합.java
@@ -0,0 +1,43 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+import javax.swing.text.html.parser.DTD;
+
+import java.util.HashMap;
+import java.util.Map;
+public class Main{
+
+    public static void main(String[] args) throws IOException{        
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine()," ");
+
+        int n = Integer.parseInt(st.nextToken()); // A집합의 원소의 개수
+        int m = Integer.parseInt(st.nextToken()); // B집합의 원소의 개수
+
+        int cnt = 0;
+        
+        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+        st = new StringTokenizer(br.readLine()," ");
+        while(st.hasMoreElements()){
+            map.put(Integer.parseInt(st.nextToken()), 1);
+        }
+        st = new StringTokenizer(br.readLine()," ");
+        
+        while(st.hasMoreElements()){
+            int tmp = Integer.parseInt(st.nextToken());
+            if(map.containsKey(tmp))
+                map.put(tmp, 0);
+            else
+                map.put(tmp, 1);
+        }
+
+        for(Map.Entry<Integer, Integer> tmp: map.entrySet()){
+            if(tmp.getValue() != 0)
+                cnt++;
+        }
+        System.out.println(cnt);
+   
+    }
+}

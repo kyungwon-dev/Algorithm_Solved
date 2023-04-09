99535f641c24c1485e8aa3b8e2f8229c28cfbc03
kyungwon oh <39756239+kyungwon-dev@users.noreply.github.com>
2023-04-09 16:22:57 +0900

    [Silver III] Title: 수열, Time: 284 ms, Memory: 23784 KB -BaekjoonHub

---
+++ 백준/Silver/2559. 수열/수열.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+public class Main{
+
+    public static void main(String[] args) throws IOException{        
+        // 입력
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine()," ");
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+
+        // 사용할 변수
+        int[] nums = new int[n+1];
+        int idx = 1; 
+        int answer = Integer.MIN_VALUE;
+
+        st = new StringTokenizer(br.readLine(), " ");
+
+        while(st.hasMoreTokens()){
+            nums[idx] = nums[idx - 1] + Integer.parseInt(st.nextToken());
+            idx++;
+        }
+        for(int i=k;i<=n;i++){
+            answer = Math.max(answer, nums[i] - nums[i - k]);
+        }
+        System.out.println(answer);
+    }
+
+}

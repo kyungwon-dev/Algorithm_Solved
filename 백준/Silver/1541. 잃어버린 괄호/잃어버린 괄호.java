a081e1834e9b5279a3e5ddd0e46d5338f2ddeb49
kyungwon oh <39756239+kyungwon-dev@users.noreply.github.com>
2023-04-23 15:34:17 +0900

    [Silver II] Title: 잃어버린 괄호, Time: 128 ms, Memory: 14248 KB -BaekjoonHub

--- 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.java
+++ 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.java
@@ -7,10 +7,17 @@ public class Main{
     public static void main(String[] args) throws IOException{        
         // 입력
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int answer = 0;        
-
         StringTokenizer minusTokneizer = new StringTokenizer(br.readLine(), "-");
+
         int loop = 0;
+        int answer = 0;        
+        
+        /*
+         * 주어지는 문자열의 형태
+         * - 30+20-10+30......-10+20..
+         * 따라서, '-'를 기준으로 자르고, '+'로 이어진 수식에 대해 수행.
+         * 모든 '+' 기호에 대해 진행 후, '-' 를 수행하면 가장 작은 값을 구할 수 있다.
+         */
         while(minusTokneizer.hasMoreTokens()){
             StringTokenizer plusTokenizer = new StringTokenizer(minusTokneizer.nextToken(), "+");
             while(plusTokenizer.hasMoreTokens()){
@@ -21,8 +28,8 @@ public class Main{
             }
             loop++;
         }
-        System.out.println(answer);
 
+        System.out.println(answer);
     }
 
 }

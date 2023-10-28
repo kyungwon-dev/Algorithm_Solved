a076be72a2223fb8afaf806e70707b9f6577518b
kyungwon oh <39756239+kyungwon-dev@users.noreply.github.com>
2023-10-28 19:27:04 +0900

    [level unrated] Title: 수 조작하기 1, Time: 0.17 ms, Memory: 4.19 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181926. 수 조작하기 1/수 조작하기 1.cpp
@@ -0,0 +1,25 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int n, string control) {
+    int answer = n;
+    for(char c : control){
+        switch(c){
+            case 'w':
+                answer+=1;
+                break;
+            case 's':
+                answer-=1;
+                break;
+            case 'd':
+                answer+=10;
+                break;
+            default:
+                answer-=10;
+                break;
+        }
+    }
+    return answer;
+}

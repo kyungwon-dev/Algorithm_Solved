ea9cec3b953160d57dc8c52ffbefd1aa27536e8e
kyungwon oh <39756239+kyungwon-dev@users.noreply.github.com>
2023-11-02 00:19:03 +0900

    [level unrated] Title: 부분 문자열, Time: 0.01 ms, Memory: 4.03 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181842. 부분 문자열/부분 문자열.cpp
@@ -0,0 +1,11 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(string str1, string str2) {
+    int answer = 0;
+    if(str1.size() > str2.size()) return 0;
+    if(str2.find(str1) == string::npos) return 0;
+    return 1;
+}

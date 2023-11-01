017b5a7b89f59d8b0747e93836d3d19fa1ebc289
kyungwon oh <39756239+kyungwon-dev@users.noreply.github.com>
2023-11-01 23:22:54 +0900

    [level unrated] Title: 주사위 게임 1, Time: 0.01 ms, Memory: 4.17 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181839. 주사위 게임 1/주사위 게임 1.cpp
@@ -0,0 +1,14 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int a, int b) {
+    int answer = 0;
+    if(a%2 == 1 && b%2 == 1){
+        return a*a + b*b;
+    } else if(a%2 == 1 || b%2 == 1){
+        return 2 * (a+b);
+    }
+    return a>b ? a-b : b-a;
+}

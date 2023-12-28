918701ea29e856f2231a38ef59046c4fcb77cac4
kyungwon oh <39756239+kyungwon-dev@users.noreply.github.com>
2023-12-28 21:18:37 +0900

    [level unrated] Title: 배열 만들기 5, Time: 5.82 ms, Memory: 104 MB -BaekjoonHub

--- 프로그래머스/unrated/181912. 배열 만들기 5/README.md
+++ 프로그래머스/unrated/181912. 배열 만들기 5/README.md
@@ -1,10 +1,10 @@
 # [level unrated] 배열 만들기 5 - 181912 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181912) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181912?language=java) 
 
 ### 성능 요약
 
-메모리: 11.5 MB, 시간: 1.76 ms
+메모리: 104 MB, 시간: 5.82 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 12월 4일 21:15:51
+2023년 12월 4일 21:18:36
 
 ### 문제 설명
 
---
+++ 프로그래머스/unrated/181912. 배열 만들기 5/배열 만들기 5.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(String[] intStrs, int k, int s, int l) {
+        List<Integer> answer = new ArrayList<Integer>();
+        for(String str : intStrs){
+            int num = Integer.valueOf(str.substring(s, s+l));
+            if( num > k)
+                answer.add(num);
+        }
+
+        return answer.stream().mapToInt(i->i).toArray();
+    }
+}

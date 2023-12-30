a221611c508b1b74cd1fac7305c8e6882fdcddba
kyungwon oh <39756239+kyungwon-dev@users.noreply.github.com>
2023-12-30 14:09:52 +0900

    [level unrated] Title: 수열과 구간 쿼리 3, Time: 0.16 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/unrated/181924. 수열과 구간 쿼리 3/수열과 구간 쿼리 3.cpp
+++ 프로그래머스/unrated/181924. 수열과 구간 쿼리 3/수열과 구간 쿼리 3.cpp
@@ -4,14 +4,6 @@
 using namespace std;
 
 vector<int> solution(vector<int> arr, vector<vector<int>> queries) {
-    for(int i=0;i<queries.size();i++){
-        int prev = queries[i][0];
-        int next = queries[i][1];
-        int tmp = 0;
-        tmp = arr[prev];
-        arr[prev] = arr[next];
-        arr[next] = tmp;
-    }
-    
-    return arr;
+    vector<int> answer;
+    return answer;
 }

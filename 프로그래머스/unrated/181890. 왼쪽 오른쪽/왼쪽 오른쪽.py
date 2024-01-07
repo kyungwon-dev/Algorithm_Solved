ccea126fadc9b488aca933cca3c4ceaf01b830c4
kyungwon oh <39756239+kyungwon-dev@users.noreply.github.com>
2024-01-08 00:45:22 +0900

    [level unrated] Title: 왼쪽 오른쪽, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181890. 왼쪽 오른쪽/왼쪽 오른쪽.py
@@ -0,0 +1,9 @@
+def solution(str_list):
+    answer = []
+    for i in range(len(str_list)):
+        if str_list[i] == "r":
+            return str_list[i+1:]
+        elif str_list[i] == "l":
+            return str_list[:i]
+        
+    return []

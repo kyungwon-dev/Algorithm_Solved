d55e62c1835ef7d48387b3319ac35ea5852d117f
kyungwon oh <39756239+kyungwon-dev@users.noreply.github.com>
2024-01-31 21:11:32 +0900

    [level 0] Title: 전국 대회 선발 고사, Time: 0.05 ms, Memory: 4.17 MB -BaekjoonHub

---
+++ 프로그래머스/0/181851. 전국 대회 선발 고사/전국 대회 선발 고사.cpp
@@ -0,0 +1,25 @@
+#include <string>
+#include <vector>
+#include <map>
+using namespace std;
+
+int solution(vector<int> rank, vector<bool> attendance) {
+    int answer = 0;
+    int mul = 10000;
+    map<int, bool> rankMap;
+    map<int, int> indexMap;
+    for(int i=0;i<rank.size();i++){
+        rankMap.insert(make_pair(rank[i], attendance[i]));
+        indexMap.insert(make_pair(rank[i], i));
+    }
+    map<int, bool>::iterator iter;
+    
+    for (iter = rankMap.begin(); iter != rankMap.end(); iter++) {
+        if(iter->second){
+            answer = answer + (mul * indexMap[iter->first]);
+            mul /= 100;
+        }
+        
+    } 
+    return answer;
+}

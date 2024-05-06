80651186f980bc653588514d9ab3b7e5702d3c4b
kyungwon oh <39756239+kyungwon-dev@users.noreply.github.com>
2024-05-06 23:29:07 +0900

    [level 2] Title: 점 찍기, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/140107. 점 찍기/점 찍기.py
+++ 프로그래머스/2/140107. 점 찍기/점 찍기.py
@@ -4,13 +4,4 @@ def solution(k, d):
     for i in range(0, d+1, k):
         if (d**2 - i**2) >= 0 :
             answer += int(math.sqrt(d**2 - i**2)) // k + 1
-        else:
-            continue
-        # print(int(math.sqrt(d**2 - i**2)) // k + 1)
-        # if 
-        # for j in range(0, d+1, k):
-        #     if (i**2) + (j**2) <= (d**2):
-        #         answer+=1
-        #     else:
-        #         break
     return answer

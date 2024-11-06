ae54572cc7682ff7a0cea148172169ce55f240b1
kyungwon oh <39756239+kyungwon-dev@users.noreply.github.com>
2024-11-06 21:31:02 +0900

    [level 3] Title: 스티커 모으기(2), Time: 58.60 ms, Memory: 16.6 MB -BaekjoonHub

---
+++ 프로그래머스/3/12971. 스티커 모으기（2）/스티커 모으기（2）.py
@@ -0,0 +1,23 @@
+def solution(sticker):
+    answer = 0
+    
+    # 3개만 있는 경우 가장 큰 것만 떼면 됨
+    if len(sticker) <= 3:
+        return max(sticker)
+
+    # [14, 6, 5, 11, 3, 9, 2, 10]
+    # [14, 14(i-1), 19(i + i-2), 11+14(i + i-2), ...]
+    
+    # 첫번째 스티커를 떼는 케이스
+    dp_f = [0] + sticker[:-1] # 마지막은 제외
+    for i in range(2, len(sticker)):
+        dp_f[i] = max(dp_f[i-1], dp_f[i] + dp_f[i-2])
+    
+    # 두번째 스티커를 떼는 케이스
+    dp_s = [0] + sticker[1:] # 마지막은 제외
+    for i in range(2, len(sticker)):
+        dp_s[i] = max(dp_s[i-1], dp_s[i] + dp_s[i-2])
+    
+    answer = max(dp_f[-1], dp_s[-1])
+
+    return answer
